89226df7379a952879dcdfb16605b16f5a6b0c60
sya-feera <113223505+sya-feera@users.noreply.github.com>
2023-10-20 08:13:58 +0700

    Add files via upload

---
+++ src/pretest/pemvis/JOPtionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pretest.pemvis;
+
+/**
+ *
+ * @author firak
+ */
+class JOPtionPane {
+
+    static void showMessageDialog(String string, String string0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
